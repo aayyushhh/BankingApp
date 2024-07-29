f46c52e6745d7057edbd4759c2a440755d2320d9
Ayush.Dodake <Ayush.Dodake@DELL1558>
2024-07-29 22:22:39 +0530

    Add project files.

---
+++ BankingApp/Account.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp
+{
+    internal class Account
+    {
+
+        private double _accountNumber;
+        private string _accountName;
+        private string _accountBankName;
+        public const int MIN_BALANCE = 500;
+        private double _accountBalance;
+        private double _adhaarNumber;
+
+
+
+        public Account(int accountNumber, string accountName, string accountBankName)
+        {
+            _accountNumber = accountNumber;
+            _accountName = accountName;
+            _accountBankName = accountBankName;
+            _accountBalance = MIN_BALANCE;
+        }
+
+        public Account(int accountNumber, string accountName, string accountBankName, double accountBalance) : this(accountNumber, accountName, accountBankName)
+        {
+            _accountBalance = CheckBalance(accountBalance);
+        }
+
+        public Account(int accountNumber, double adhaarNumber, string accountName, string accountBankName) : this(accountNumber, accountName, accountBankName)
+        {
+
+        }
+        public Account(int accountNumber, double adhaarNumber, string accountName, string accountBankName, double accountBalance)
+        {
+            _accountNumber = accountNumber;
+            _accountName = accountName;
+            _accountBankName = accountBankName;
+            _accountBalance = accountBalance;
+            _adhaarNumber = adhaarNumber;
+
+
+
+
+        }
+
+        public double Deposit(double amount)
+        {
+
+            _accountBalance += amount;
+            return _accountBalance;
+
+        }
+
+        public double CheckBalance(double input)
+        {
+            if (input < MIN_BALANCE)
+            {
+                input = MIN_BALANCE;
+            }
+            return input;
+        }
+        public double Withdrawal(double amount)
+        {
+            if (Account.MIN_BALANCE > amount)
+            {
+                _accountBalance -= amount;
+                return _accountBalance;
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+                return _accountBalance;
+
+            }
+
+
+        }
+
+        public static Account GetAccountById(Account[] accounts, int accountId)
+        {
+            foreach (Account account in accounts)
+            {
+                if (account._accountNumber == accountId)
+                {
+                    return account;
+                }
+            }
+            return null;
+        }
+
+        public Account HighestBalance(Account[] accounts)
+        {
+            double highestBalance = 0;
+            Account highestAccount=null;
+
+            foreach (Account account in accounts)
+            {
+                if (account._accountNumber > highestBalance)
+                {
+                    highestBalance = account._accountNumber;
+                    highestAccount = account;
+                }
+
+
+            }
+            return highestAccount;
+
+        }
+
+
+        }
+
+
+
+
+
+    }
+
+
---
+++ BankingApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace BankingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account(1,123456, "Sachin", "SBI",100 );
+            Account account2 = new Account(2,678901, "Rahul", "HDFC", 800);
+
+            Account[] accounts = new Account[] { account1, account2 };
+
+            Console.WriteLine("Enter the account ID you want to access:");
+            int accountId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Adhaar number:");
+            int adharId = int.Parse(Console.ReadLine());
+
+            Account selectedAccount = Account.GetAccountById(accounts, accountId);
+
+            if (selectedAccount != null && adharId != 0)
+            {
+                try
+                {
+                    Console.WriteLine("Do you want to deposit or withdraw money? (Enter 'd' for deposit and 'w' for withdraw):");
+                    char choice = char.Parse(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 'd':
+                            Console.WriteLine("Enter the amount you want to deposit:");
+                            double depositAmount = double.Parse(Console.ReadLine());
+                            double newBalanceAfterDeposit = selectedAccount.Deposit(depositAmount);
+                            Console.WriteLine("Bank balance is Rs " + newBalanceAfterDeposit);
+                            break;
+                        case 'w':
+                            Console.WriteLine("Enter the amount you want to withdraw:");
+                            double withdrawalAmount = double.Parse(Console.ReadLine());
+                            double newBalanceAfterWithdrawal = selectedAccount.Withdrawal(withdrawalAmount);
+                            Console.WriteLine("Balance left is Rs " + newBalanceAfterWithdrawal);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice. Please enter 'd' to deposit or 'w' to withdraw.");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Account with ID " + accountId + " not found.");
+            }
+        }
+
+
+
+
+
+
+    }
+
+
+}
+
